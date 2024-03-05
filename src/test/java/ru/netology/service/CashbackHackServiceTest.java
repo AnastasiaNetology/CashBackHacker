127afca3c9a654226a9448c8398e0ac536ea0803
Anastasia <barabanova.aaa@gmail.com>
2024-03-05 20:13:44 +0300

    added new tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,10 +1,9 @@
 package ru.netology.service;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.testng.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
 
-    @Test
+    @org.testng.annotations.Test
     public void testRemain() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
@@ -13,12 +12,36 @@ public class CashbackHackServiceTest {
         assertEquals(actual, expected);
     }
 
-    @Test
-    public void testRemainWithCashback() {
+    @org.testng.annotations.Test
+    public void testRemainMore() {
         CashbackHackService service = new CashbackHackService();
         int amount = 500;
         int actual = service.remain(amount);
         int expected = 500;
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+
+    public void testRemainMin() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+
+    public void testRemainMax() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1200;
+        int actual = service.remain(amount);
+        int expected = 800;
+
         assertEquals(actual, expected);
     }
 }
